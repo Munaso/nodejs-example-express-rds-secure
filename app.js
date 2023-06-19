cd876107639dc5d996b1d10d9bccefe4c6670ba8
임은호 <>
2023-06-19 23:50:03 +0900

    test

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var app = express();
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -37,7 +38,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // render the error page !
   res.status(err.status || 500);
   res.render("error");
 });
